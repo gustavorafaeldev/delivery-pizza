66f19852c92ddcab7b52848e9d46652029c5a24c
gustavorafaeldev <105467235+gustavorafaeldev@users.noreply.github.com>
2022-06-14 20:04:47 -0300

    Atualização script

--- script.js
+++ script.js
@@ -100,12 +100,30 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     closeModal();
 });
 
+c('.menu-openner').addEventListener('click', ()=> {
+    if(cart.length > 0) {
+        c('aside').style.left = '0';
+    }
+});
+
+c('.menu-closer').addEventListener('click', ()=> {
+    c('aside').style.left = '100vw';
+});
+
 function updateCart() {
+    c('.menu-openner span').innerHTML = cart.length;
     if(cart.length > 0) {
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
+
+        let subTotal = 0;
+        let desconto = 0;
+        let total = 0;
+
         for(let i in cart) {
             let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+
+            subTotal += pizzaItem.price * cart[i].qt;
 
             let pizzaSizeName;
             switch(cart[i].size) {
@@ -130,10 +148,34 @@ function updateCart() {
            
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
 
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=> {
+                if(cart[i].qt > 1){
+                    cart[i].qt--;
+                } else { 
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt++;
+                updateCart();
+            });
+
             c('.cart').append(cartItem);
-        
         }
+
+        desconto = subTotal * 0.1;
+        total = subTotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subTotal.toFixed(2)}`;
+
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
+
     } else {
         c('aside').classList.remove('show');
+        c('aside').style.left = '100vw';
     }
 }
